8ad53ae27c03f2140c2c854f5e00f63105f329f0
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2020-07-29 19:47:45 +0300

    Add files via upload

---
+++ if_else_statement.dart
@@ -0,0 +1,33 @@
+ /*
+  Control flows:
+   1. IF and ELSE
+   2. Conditional Expressions
+   3. SWITCH and CASE Statements
+
+*/
+void main(){
+  var pay = 10000;
+  
+  // IF-ELSE statement
+  if (pay > 29000){ // Execute the code if condition is true
+    print("You got promotion. Congratulations!");
+  } else { // Execute code here if condiiton is false
+    print("You need to work hard!!!");
+  }
+
+ // IF ELSE IF Ladder statement
+ var marks = 95;
+  
+  if (marks >=90 && marks <100){
+    print ("A+ grade");
+  } else if (marks >=80 && marks <90){
+    print("B grade");
+  } else if (marks >=70 && marks <80){
+    print("C grade");
+  } else if (marks >=50 && marks <70){
+    print("C- grade");
+  } else if (marks < 50){
+    print("You failed");
+  }
+
+}
