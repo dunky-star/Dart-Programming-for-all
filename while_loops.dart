70c58b6f7e7243113c50df2c0630fcaab3c128b7
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2020-07-31 12:25:21 +0300

    Update while_loops.dart

--- while_loops.dart
+++ while_loops.dart
@@ -3,7 +3,7 @@
    Used for iteration when incase you don't know how many times your code needs to be executed.
 */
 void main(){
-  var i = 0;
+  var i = 1;
   while(i < 20) {
     if (i%2 == 0){
       print(i);
