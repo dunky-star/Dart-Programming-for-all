68df22066c0dac1575f839a0d7753c387e465c90
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2020-08-01 11:32:16 +0300

    Update nested_for_loop.dart

--- nested_for_loop.dart
+++ nested_for_loop.dart
@@ -7,17 +7,21 @@ void main(){
   
   dunkyOuterLoop: for (var i = 1; i <= 6; i++){
     for (var j = 1; j <= 6; j++){
-      print("$i $j");
-      if (i == 3 && j == 3){
+      
+      if (i == 2 && j == 2){
+        continue dunkyOuterLoop;
+      } else if (i == 3 && j == 3){
         break dunkyOuterLoop;  //BREAK keyword here is only applicable                               
-		                      //to its nearest parent loop. i.e. 
+		              //to its nearest parent loop. i.e. 
                              //the inner loop. If you want to break
                             //from both inner and outer loop then 
-                           //you need to give the outer loop a name.
+                           //you need to give outer loop a name.
       }
+      print("$i $j");
     }
   }
       
 }
+
 
 
