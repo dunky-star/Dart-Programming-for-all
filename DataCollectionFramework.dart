3e3e3065de4eae541e3808b26a27e5d774c3392b
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2020-08-04 15:53:21 +0300

    Add files via upload

---
+++ DataCollectionFramework.dart
@@ -0,0 +1,47 @@
+/*
+ * List (Fixed-length list): In Dart, ARRAY is knwon as LIST
+ * Growable list
+ * set (Implementation: HashSet)
+ * Maps (Implementation: HashMap)
+ * @Opiyo Geoffrey Duncan
+*/
+
+void main() {
+  
+  List<int> numbersList = List(5); //Fixed-length list.
+  numbersList[0] =100; // Insert operation on numbersList
+  numbersList[1] =200;
+  numbersList[2] =300;
+  numbersList[3] =400;
+  numbersList[4] =500; 
+  
+  numbersList[0] = 1000; // Update operation  
+  numbersList[4] = null; // Delete operation
+  
+  /*
+  numbersList.remove(73); // Not supported in fixed-length list  
+  numbersList.add(24);    // Not supported in fixed-length list  
+  numbersList.removeAt(3); // Not supported in fixed-length list  
+  numbersList.add(24);     // Not supported in fixed-length list  
+  numbersList.add(24);     // Not supported in fixed-length list  
+  */
+  //printing numbersList
+  numbersList.forEach((element) => print(element)); // Using Lambda expression to print elements in the List.
+  print("");
+  for (int i =0; i < numbersList.length; i++){ // Using primitive for loop to print.
+    print(numbersList[i]);
+  }
+  print("");
+  for (int element in numbersList){ // Using individual element (object).
+    print(element);
+  }
+  
+}
+
+	       
+  
+  
+
+	       
+  
+  
