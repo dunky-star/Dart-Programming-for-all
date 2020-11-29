4c874938da029fc449ea58d54dc45fa68ba60fa6
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2020-11-29 20:12:57 +0300

    Add files via upload

---
+++ inter_process_communication.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+import 'dart:convert';
+
+void main(List<String> arguments) {
+  //Linux Specific
+  Process.start('cat', []).then((Process process) {
+    //console DIR
+    process.stdout.transform(utf8.decoder).listen((data) {
+      print(data);
+    });
+
+    //Send to the process
+    process.stdin.writeln('Hello, this is inter-process communication in Dart');
+
+    //Stop the process
+    Process.killPid(process.pid);
+
+    //get the exit code
+    process.exitCode.then((int code) {
+      print('Exit code: ${code}');
+
+      //exit
+      exit(0);
+    });
+  });
+}
