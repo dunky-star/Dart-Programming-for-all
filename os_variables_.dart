08998b0222da15f477a5db18241ca89cb6ccf6a3
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2020-11-29 19:44:55 +0300

    Update os_variables_.dart

--- os_variables_.dart
+++ os_variables_.dart
@@ -25,7 +25,6 @@ void main(List<String> arguments) {
   print('\n\r');
    // List all files in a directory - Linux specific
   Process.run('ls', ['-l']).then((ProcessResult results) {
-    // notepad C:\windows\System32\notepad.exe
     print(results.stdout);
     print('Exit code: ${results.exitCode}'); // 0 is good
   });
