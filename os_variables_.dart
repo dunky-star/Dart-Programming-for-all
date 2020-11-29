5874a72d95916b4c307d3296f3f764b68093a261
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2020-11-29 19:23:50 +0300

    Update os_variables_.dart

--- os_variables_.dart
+++ os_variables_.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
+import 'dart:convert';
 
 void main(List<String> arguments) {
+  // Displaying operating system and version
   print('OS: ${Platform.operatingSystem} ${Platform.version}');
 
   if (Platform.isWindows) {
@@ -10,9 +12,16 @@ void main(List<String> arguments) {
   }
   print('Path: ${Platform.script.path}');
   print('Dart: ${Platform.executable}');
-
+  // Displaying system path
   print('Env:');
   Platform.environment.keys.forEach((key) {
     print('${key} ${Platform.environment[key]}');
+  });
+  print('\n\r');
+  // List all files in a directory - Linux specific
+  Process.run('ls', ['-l']).then((ProcessResult results) {
+    // notepad C:\windows\System32\notepad.exe
+    print(results.stdout);
+    print('Exit code: ${results.exitCode}'); // 0 is good
   });
 }
